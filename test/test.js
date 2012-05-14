1184630eb4299897a1ad17e06f9d366dba0bc702
Drew Young <drewyoung1@gmail.com>
2012-05-14 17:10:21 -0400

    Make plugin system work ( activating and deactivating for seperate documents ) and get javascript to execute

--- test/test.js
+++ test/test.js
@@ -7,9 +7,15 @@ var ignoreFiles = [
 	'reparse.js'
 ];
 
+var run = process.argv.slice(2);
+
+run = run.length ? run : undefined;
+
 fs.readdir(__dirname, function(err, files) {
 	files.filter(function(file) {
 		return file && /\.js$/.test(file) && file !== 'test.js' && ignoreFiles.indexOf(file) === -1;
+	}).filter(function(file) {
+		return typeof(run) === 'object' && run.indexOf(file.replace(/\.js$/, '')) >= 0;
 	}).forEach(function(name) {
 		var test, tests, testName;
 		console.log('\n==================Running %s=====================', name.replace(/\.js$/, ''));
@@ -23,7 +29,7 @@ fs.readdir(__dirname, function(err, files) {
 				var doc;
 				
 				console.log('\n===================' + testName + ( typeof(test.html) === 'string' ? ' "' + test.html + '"' : '' ) + '=======================\n');
-				doc = new html.Document(test.html);
+				doc = new html.Document(test.html, test.plugins);
 				
 				if(typeof(test.run) === 'function') {
 					test.run(doc);
