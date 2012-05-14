18582c2d67b1b7a26d262922d5d4818e7566e78a
Drew Young <drewyoung1@gmail.com>
2012-05-13 21:28:11 -0400

    Add some more tests for doctype

--- test/doctype.js
+++ test/doctype.js
@@ -2,10 +2,18 @@ var path = require('path');
 
 var html = require(path.join(__dirname, '../lib/index'));
 
-console.log("\n================DocType <!DOCTYPE html>====================\n");
+console.log("\n================DocType <!DOCTYPE html><html />====================\n");
 
 var doc = new html.Document();
 
 doc.innerHTML = '<!DOCTYPE html><html />';
+
+console.log(doc.innerHTML);
+
+console.log("\n================DocType ''====================\n");
+
+var doc = new html.Document();
+
+doc.innerHTML = '';
 
 console.log(doc.innerHTML);
